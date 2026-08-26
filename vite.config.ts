15d99b1a715f57f226a9fd65dfcce820a7f55a00
A. Farkhan Dwi Kusuma <afarkhandwikusuma.id@gmail.com>
2026-08-27 01:47:56 +0700

    remove puppeteer

--- vite.config.ts
+++ vite.config.ts
@@ -2,36 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-import prerender from '@prerenderer/rollup-plugin';
-import PuppeteerRenderer from '@prerenderer/renderer-puppeteer';
 
 export default defineConfig(() => {
   return {
     plugins: [
       react(), 
-      tailwindcss(),
-      prerender({
-        routes: [
-          '/', 
-          '/profil/sejarah', 
-          '/profil/visi-misi', 
-          '/profil/struktur', 
-          '/profil/akreditasi', 
-          '/akademik/kurikulum', 
-          '/akademik/pembelajaran', 
-          '/fasilitas', 
-          '/galeri', 
-          '/alumni/pendataan', 
-          '/alumni/statistik'
-        ],
-        renderer: new PuppeteerRenderer({
-          // Wait 5 seconds to ensure React Suspense and Supabase fetches are complete
-          renderAfterTime: 5000
-        }),
-        server: {
-          port: 3000
-        }
-      })
+      tailwindcss()
     ],
     resolve: {
       alias: {
